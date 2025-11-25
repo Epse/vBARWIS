e652bd1b4050aca9b8f4e292b8f58de0f78f562b
Stef Pletinck <s@stefpletinck.be>
2025-11-25 13:00:45 +0100

    Add tick marks to roses
    No numbers yet, but we get there

--- src/meteo_brussels/widgets/wind_rose/__init__.py
+++ src/meteo_brussels/widgets/wind_rose/__init__.py
@@ -1,6 +1,6 @@
 from math import floor
 import math
-from PySide6.QtWidgets import QWidget, QGraphicsScene, QGraphicsView, QVBoxLayout, QGraphicsEllipseItem, QGridLayout, QSizePolicy, QPushButton, QLabel, QFrame
+from PySide6.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsEllipseItem, QGridLayout, QSizePolicy, QPushButton, QLabel, QFrame, QGraphicsLineItem
 from PySide6.QtGui import QBrush, QColor, QPen, QResizeEvent, QShowEvent, QIcon
 from PySide6.QtCore import QEvent, Qt, QLineF, Signal
 from widgets import DARK_GREEN, BLUE
@@ -11,179 +11,200 @@ from .arc import QGraphicsArcItem
 
 
 def transform_angle(angle: float) -> int:
-	"""
-	Takes a float angle in degrees,
-	and turns it into a Qt angle.
+    """
+    Takes a float angle in degrees,
+    and turns it into a Qt angle.
 
-	Qt angles follow these invariants:
-	- angles are integers, in 16ths of a degree
-	- 0° is at 3 o'clock
-	- Positive angles go counter-clockwise
+    Qt angles follow these invariants:
+    - angles are integers, in 16ths of a degree
+    - 0° is at 3 o'clock
+    - Positive angles go counter-clockwise
 
-	Wind rose angles go differently,
-	0 is at the top and positive angles go to the right
-	"""
-	angle = -angle + 90.0
-	angle = angle % 360.0
+    Wind rose angles go differently,
+    0 is at the top and positive angles go to the right
+    """
+    angle = -angle + 90.0
+    angle = angle % 360.0
 
-	return floor(angle * 16) # Integer representation
+    return floor(angle * 16) # Integer representation
 
 def normalise_heading(heading: int) -> int:
-	"""same as above, but only corrects the transform"""
-	return (- heading + 90) % 360
+    """same as above, but only corrects the transform"""
+    return (- heading + 90) % 360
 
 
 def line_for_wind_heading(heading: int) -> QLineF:
-	normalised_heading = normalise_heading(heading)
-	y_heading = -math.sin(math.radians(normalised_heading)) * 5 + 5
-	x_heading = math.cos(math.radians(normalised_heading)) * 5 + 5
-	return QLineF(5, 5, x_heading, y_heading)
+    normalised_heading = normalise_heading(heading)
+    y_heading = -math.sin(math.radians(normalised_heading)) * 5 + 5
+    x_heading = math.cos(math.radians(normalised_heading)) * 5 + 5
+    return QLineF(5, 5, x_heading, y_heading)
 
 
-# TODO verify its correct... I've seen some things going bad...
+# TODO verify its correct... I've seen some things going bad... VRB btn 300 and 000, heading 320 seems to give bad results
 class WindRose(QFrame):
-	pie_width = 10.0
-	_sensor_reading: SensorReading
-	"""Width of the slice showing current wind direction"""
+    pie_width = 10.0
+    _sensor_reading: SensorReading
+    """Width of the slice showing current wind direction"""
+    _tick_items: list[QGraphicsEllipseItem | QGraphicsLineItem] = []
 
-	popped_out = Signal(str)
-
-
-	def __init__(self, show_debug_lines: bool = False, can_pop_out: bool = True):
-		super().__init__()
-
-		self.show_debug_lines = show_debug_lines
-
-		if can_pop_out:
-			self.setFrameStyle(QFrame.Shape.StyledPanel | QFrame.Shadow.Plain)
-			self.setLineWidth(2)
-
-		self._scene = QGraphicsScene()
-		self._scene.setBackgroundBrush(self.palette().base())
-
-		self.central = QGraphicsEllipseItem(0, 0, 10, 10)
-		self.central.setBrush(QBrush(DARK_GREEN))
-		self.central.setPen(Qt.PenStyle.NoPen)
-
-		arc_pen = QPen(BLUE, 1.0, c=Qt.PenCapStyle.FlatCap)
-		self.left_arc = QGraphicsArcItem(0, 0, 10, 10)
-		self.left_arc.setPen(arc_pen)
-		self.left_arc.setBrush(Qt.BrushStyle.NoBrush)
-		self.right_arc = QGraphicsArcItem(0, 0, 10, 10)
-		self.right_arc.setPen(arc_pen)
-		self.right_arc.setBrush(Qt.BrushStyle.NoBrush)
-
-		self._scene.addItem(self.central)
-		self._scene.addItem(self.left_arc)
-		self._scene.addItem(self.right_arc)
-
-		# Temporary, until I figure out tickmarks
-		pen = QPen(self.palette().windowText(), 0.0)
-		self._scene.addLine(0, 0, 10, 10, pen)
-		self._scene.addLine(0, 10, 10, 0, pen)
-		self._scene.addLine(0, 5, 10, 5, pen)
-		self._scene.addLine(5, 0, 5, 10, pen)
-
-		# Add placeholder orientation lines
-		self.heading_line = self._scene.addLine(5, 5, 0, 0, QPen(QColor.fromString("pink"), 0.0))
-
-		debug_pen = QPen(QColor.fromString("cyan"), 0.0)
-		self.wind_lower_line = self._scene.addLine(5, 5, 0, 0, debug_pen)
-		self.wind_upper_line = self._scene.addLine(5, 5, 0, 0, debug_pen)
-
-		self.heading_line.setVisible(self.show_debug_lines)
-		self.wind_lower_line.setVisible(self.show_debug_lines)
-		self.wind_upper_line.setVisible(self.show_debug_lines)
-
-		self.view = QGraphicsView(self._scene)
-		self._layout = QGridLayout()
-
-		self.title = BigLabel(scaling=1.5)
-		self._layout.addWidget(self.view, 0, 0, 1, 4)
-		self._layout.addWidget(self.title, 0, 0, 1, 1, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-		
-		if can_pop_out:
-			popout_button = QPushButton(QIcon.fromTheme(QIcon.ThemeIcon.WindowNew), "")
-			popout_button.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-			popout_button.pressed.connect(lambda: self.popped_out.emit("runway-" + self._sensor_reading.label))
-			self._layout.addWidget(popout_button, 0, 3, 1, 1, alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
-
-		self._reading_widget = WindReading()
-		self._layout.addWidget(self._reading_widget, 1, 0, 1, 4, alignment=Qt.AlignmentFlag.AlignHCenter)
-
-		# variability
-		self._layout.addWidget(QLabel("VRB BTN", alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignCenter), 2, 0)
-		self._variable_lower = BigLabel(scaling=1.5)
-		self._layout.addWidget(self._variable_lower, 2, 1, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
-		self._layout.addWidget(QLabel("AND", alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignCenter), 2, 2)
-		self._variable_upper = BigLabel(scaling=1.5)
-		self._layout.addWidget(self._variable_upper, 2, 3, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
-
-		self._layout.setColumnStretch(0, 1)
-		self._layout.setColumnStretch(1, 1)
-		self._layout.setColumnStretch(2, 1)
-		self._layout.setColumnStretch(3, 2)
-		self.setLayout(self._layout)
-
-	def set_wind(self, reading: SensorReading) -> None:
-		self._sensor_reading = reading
-
-		self._render()
-
-	def set_debug(self, val: bool) -> None:
-		self.show_debug_lines = val
-		self.heading_line.setVisible(self.show_debug_lines)
-		self.wind_lower_line.setVisible(self.show_debug_lines)
-		self.wind_upper_line.setVisible(self.show_debug_lines)
-
-	def _render(self):
-		self.title.setText(self._sensor_reading.label)
-		self._reading_widget.set_data(self._sensor_reading)
-
-		vrb_lower = (self._sensor_reading.wind_direction - self._sensor_reading.wind_direction_deviation_left) % 360
-		vrb_upper = (self._sensor_reading.wind_direction + self._sensor_reading.wind_direction_deviation_right) % 360
-		self._variable_lower.setText(f"{vrb_lower:03d}°")
-		self._variable_upper.setText(f"{vrb_upper:03d}°")
-
-		wind_angle = normalise_heading(self._sensor_reading.wind_direction)
-
-		self.heading_line.setLine(line_for_wind_heading(self._sensor_reading.wind_direction))
-		self.wind_lower_line.setLine(line_for_wind_heading(self._sensor_reading.wind_direction + self._sensor_reading.wind_direction_deviation_left))
-		self.wind_upper_line.setLine(line_for_wind_heading(self._sensor_reading.wind_direction - self._sensor_reading.wind_direction_deviation_right))
-
-		# Normalisation functions guarantee positive angles from any heading. This simplifies
-
-		self.central.setStartAngle(floor((wind_angle + self.pie_width / 2) * 16.0))
-		self.central.setSpanAngle(floor((360.0 - self.pie_width) * 16))
-
-		self.left_arc.setStartAngle(floor(wind_angle + self.pie_width / 2) * 16)
-		self.left_arc.setSpanAngle(self._sensor_reading.wind_direction_deviation_left * 16)
-
-		self.right_arc.setStartAngle(floor(wind_angle - self.pie_width / 2) * 16)
-		self.right_arc.setSpanAngle(-self._sensor_reading.wind_direction_deviation_right * 16)
-
-		self._fit()
+    popped_out = Signal(str)
 
 
-	def resizeEvent(self, event: QResizeEvent) -> None:
-		super().resizeEvent(event)
-		self._fit()
+    def __init__(self, show_debug_lines: bool = False, can_pop_out: bool = True):
+        super().__init__()
 
-	def showEvent(self, event: QShowEvent) -> None:
-		super().showEvent(event)
-		self._fit()
+        self.show_debug_lines = show_debug_lines
+
+        if can_pop_out:
+            self.setFrameStyle(QFrame.Shape.StyledPanel | QFrame.Shadow.Plain)
+            self.setLineWidth(2)
+
+        self._scene = QGraphicsScene()
+        self._scene.setBackgroundBrush(self.palette().base())
+
+        self.central = QGraphicsEllipseItem(0, 0, 10, 10)
+        self.central.setBrush(QBrush(DARK_GREEN))
+        self.central.setPen(Qt.PenStyle.NoPen)
+
+        arc_pen = QPen(BLUE, 1.0, c=Qt.PenCapStyle.FlatCap)
+        self.left_arc = QGraphicsArcItem(0, 0, 10, 10)
+        self.left_arc.setPen(arc_pen)
+        self.left_arc.setBrush(Qt.BrushStyle.NoBrush)
+        self.right_arc = QGraphicsArcItem(0, 0, 10, 10)
+        self.right_arc.setPen(arc_pen)
+        self.right_arc.setBrush(Qt.BrushStyle.NoBrush)
+
+        self._scene.addItem(self.central)
+        self._scene.addItem(self.left_arc)
+        self._scene.addItem(self.right_arc)
+
+        # Temporary, until I figure out tickmarks
+        self._draw_tick_marks()
+        #pen = QPen(self.palette().windowText(), 0.0)
+        #self._scene.addLine(0, 0, 10, 10, pen)
+        #self._scene.addLine(0, 10, 10, 0, pen)
+        #self._scene.addLine(0, 5, 10, 5, pen)
+        #self._scene.addLine(5, 0, 5, 10, pen)
+
+        # Add placeholder orientation lines
+        self.heading_line = self._scene.addLine(5, 5, 0, 0, QPen(QColor.fromString("pink"), 0.0))
+
+        debug_pen = QPen(QColor.fromString("cyan"), 0.0)
+        self.wind_lower_line = self._scene.addLine(5, 5, 0, 0, debug_pen)
+        self.wind_upper_line = self._scene.addLine(5, 5, 0, 0, debug_pen)
+
+        self.heading_line.setVisible(self.show_debug_lines)
+        self.wind_lower_line.setVisible(self.show_debug_lines)
+        self.wind_upper_line.setVisible(self.show_debug_lines)
+
+        self.view = QGraphicsView(self._scene)
+        self._layout = QGridLayout()
+
+        self.title = BigLabel(scaling=1.5)
+        self._layout.addWidget(self.view, 0, 0, 1, 4)
+        self._layout.addWidget(self.title, 0, 0, 1, 1, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        
+        if can_pop_out:
+            popout_button = QPushButton(QIcon.fromTheme(QIcon.ThemeIcon.WindowNew), "")
+            popout_button.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+            popout_button.pressed.connect(lambda: self.popped_out.emit("runway-" + self._sensor_reading.label))
+            self._layout.addWidget(popout_button, 0, 3, 1, 1, alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
+
+        self._reading_widget = WindReading()
+        self._layout.addWidget(self._reading_widget, 1, 0, 1, 4, alignment=Qt.AlignmentFlag.AlignHCenter)
+
+        # variability
+        self._layout.addWidget(QLabel("VRB BTN", alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignCenter), 2, 0)
+        self._variable_lower = BigLabel(scaling=1.5)
+        self._layout.addWidget(self._variable_lower, 2, 1, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+        self._layout.addWidget(QLabel("AND", alignment=Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignCenter), 2, 2)
+        self._variable_upper = BigLabel(scaling=1.5)
+        self._layout.addWidget(self._variable_upper, 2, 3, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+
+        self._layout.setColumnStretch(0, 1)
+        self._layout.setColumnStretch(1, 1)
+        self._layout.setColumnStretch(2, 1)
+        self._layout.setColumnStretch(3, 2)
+        self.setLayout(self._layout)
+
+    def set_wind(self, reading: SensorReading) -> None:
+        self._sensor_reading = reading
+
+        self._render()
+
+    def set_debug(self, val: bool) -> None:
+        self.show_debug_lines = val
+        self.heading_line.setVisible(self.show_debug_lines)
+        self.wind_lower_line.setVisible(self.show_debug_lines)
+        self.wind_upper_line.setVisible(self.show_debug_lines)
+
+    def _render(self):
+        self.title.setText(self._sensor_reading.label)
+        self._reading_widget.set_data(self._sensor_reading)
+
+        vrb_lower = (self._sensor_reading.wind_direction - self._sensor_reading.wind_direction_deviation_left) % 360
+        vrb_upper = (self._sensor_reading.wind_direction + self._sensor_reading.wind_direction_deviation_right) % 360
+        self._variable_lower.setText(f"{vrb_lower:03d}°")
+        self._variable_upper.setText(f"{vrb_upper:03d}°")
+
+        wind_angle = normalise_heading(self._sensor_reading.wind_direction)
+
+        self.heading_line.setLine(line_for_wind_heading(self._sensor_reading.wind_direction))
+        self.wind_lower_line.setLine(line_for_wind_heading(self._sensor_reading.wind_direction + self._sensor_reading.wind_direction_deviation_left))
+        self.wind_upper_line.setLine(line_for_wind_heading(self._sensor_reading.wind_direction - self._sensor_reading.wind_direction_deviation_right))
+
+        # Normalisation functions guarantee positive angles from any heading. This simplifies
+
+        self.central.setStartAngle(floor((wind_angle + self.pie_width / 2) * 16.0))
+        self.central.setSpanAngle(floor((360.0 - self.pie_width) * 16))
+
+        self.left_arc.setStartAngle(floor(wind_angle + self.pie_width / 2) * 16)
+        self.left_arc.setSpanAngle(self._sensor_reading.wind_direction_deviation_left * 16)
+
+        self.right_arc.setStartAngle(floor(wind_angle - self.pie_width / 2) * 16)
+        self.right_arc.setSpanAngle(-self._sensor_reading.wind_direction_deviation_right * 16)
+
+        self._fit()
 
 
-	def get_reading(self) -> SensorReading:
-		return self._sensor_reading
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        super().resizeEvent(event)
+        self._fit()
 
-	def _fit(self) -> None:
-		self.view.fitInView(0, 0, 12, 12, Qt.AspectRatioMode.KeepAspectRatio)
+    def showEvent(self, event: QShowEvent) -> None:
+        super().showEvent(event)
+        self._fit()
 
-	def event(self, e: QEvent) -> bool:
-		if e.type() == QEvent.Type.PaletteChange:
-			self._scene.setBackgroundBrush(self.palette().base())
-		return super().event(e)
 
-	def _draw_tick_marks(self) -> None:
-		pass
+    def get_reading(self) -> SensorReading:
+        return self._sensor_reading
+
+    def _fit(self) -> None:
+        self.view.fitInView(0, 0, 12, 12, Qt.AspectRatioMode.KeepAspectRatio)
+
+    def event(self, e: QEvent) -> bool:
+        if e.type() == QEvent.Type.PaletteChange:
+            self._scene.setBackgroundBrush(self.palette().base())
+            pen = QPen(self.palette().windowText(), 0.0)
+            for item in self._tick_items:
+                item.setPen(pen)
+        return super().event(e)
+
+    def _draw_tick_marks(self) -> None:
+        radius = 5
+        pen = QPen(self.palette().windowText(), 0.0)
+
+        self._tick_items.append(self._scene.addEllipse(0, 0, 2*radius, 2*radius, pen, Qt.BrushStyle.NoBrush))
+
+        for angle in range(10, 360, 10):
+            y_heading = -math.sin(math.radians(angle))
+            x_heading = math.cos(math.radians(angle))
+
+            scale = 0.5 if angle % 30 == 0 else 0.2
+
+            y_start = radius + y_heading * radius
+            y_end = radius + y_heading * (radius + scale)
+            x_start = radius + x_heading * radius
+            x_end = radius + x_heading * (radius + scale)
+
+            self._tick_items.append(self._scene.addLine(x_start, y_start, x_end, y_end, pen))
